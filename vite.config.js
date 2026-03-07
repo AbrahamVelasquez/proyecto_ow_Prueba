a5d3634bd3ee6a8e5a644e2f28902b81109b2084
AbrahamVelasquez <abrahamjv026@gmail.com>
2026-03-07 17:12:36 +0100

    Tercer Commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://abrahamvelasquez.github.io/proyecto_ow_Prueba/', // ¡Ojo! Pon el nombre exacto de tu repo en GitHub
 })
